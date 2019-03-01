bfd04f68edc48b86b745432bce58bb519ddf5347
hamer073 <hamer073@morris.umn.edu>
2019-03-01 16:16:09 -0600

    Did more thing

---
+++ drop_tables.dbf-summarize-db-promises.js
@@ -0,0 +1,58 @@
+Promise=require('bluebird')
+mysql=require('mysql');
+DBF=require('./drop_tables.dbf-setup.js');
+
+var getDatabases=function(){
+  var sql = "SHOW DATABASES";
+  return DBF.query(mysql.format(sql));
+}
+
+var processDBFs=function(queryResults){
+   var dbfs=queryResults;
+   return(Promise.all(dbfs.map(dbfToPromise)).then(processTables))
+}
+
+var processTables=function(results){
+  var descriptionPromises=results.map(tableAndDbfToPromise);
+  var allTables=Promise.all(descriptionPromises).then(function(results){return(results)});
+  return(allTables);
+}
+
+var dbfToPromise=function(dbfObj){
+  var dbf=dbfObj.Database
+  var sql = mysql.format("SHOW TABLES IN ??",dbf);
+  var queryPromise=DBF.query(sql)
+  queryPromise=queryPromise.then(function(results){return({table:results,dbf:dbf})});
+  return(queryPromise);
+}
+
+var tableAndDbfToPromise=function(obj){
+   var dbf=obj.dbf;
+   var tableObj=obj.table;
+   var key = "Tables_in_"+dbf;
+
+   var tables=tableObj.map(function(val){return(val[key])})
+
+   var describeTable=function(val,index){
+       var table=dbf+"."+val;
+       var printer=function(results){
+          var desc=results;
+          if(index==0){console.log("---|",dbf,">")};
+          console.log(".....|"+table,">");
+          desc.map(function(field){
+             console.log("\tFieldName: `"+field.Field+"` \t("+field.Type+")");
+          })
+       }
+
+       var describeSQL=mysql.format("DESCRIBE ??",table);
+       var promise=DBF.query(describeSQL).then(printer);
+       return(promise);
+   }
+   var describePromises = tables.map(describeTable);
+   return(Promise.all(describePromises))
+}
+
+var dbf=getDatabases()
+.then(processDBFs)
+.then(DBF.releaseDBF)
+.catch(function(err){console.log("DANGER:",err)});
