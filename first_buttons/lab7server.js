cb8be2f89e24862b9ed73dea697e23c7771b32d3
EthanUphoff <ethan.uphoff@gmail.com>
2019-03-06 11:15:06 -0600

    removed commented and unused code

--- first_buttons/lab7server.js
+++ first_buttons/lab7server.js
@@ -32,24 +32,20 @@ var query = function(command) {
 var result = query("select * from till_buttons;");
 result.then(function(dbfs,err){
   var dbfarr = new Array(dbfs.length);
-  console.log(dbfs[0].label)
+  //console.log(dbfs[0].label)
   dbfs.forEach(function (item, index) {
-  	console.log(item.leftPosition)
+  	//console.log(item.leftPosition)
 	dbfarr[index] = {"buttonID":item.button_id,
 		"left":item.leftPosition,
 		"top":item.topPosition,
 		"width":item.wide,
 		"label":item.label,
 		"invID":item.invID};
-	console.log(dbfarr[index])
+	//console.log(dbfarr[index])
   })
-  var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
-
-console.log(buttons[0])
-
 app.use(express.static(__dirname + '/public')); //Serves the web pages
 app.get("/buttons",function(req,res){ // handles the /buttons API
-  res.send(buttons);
+  res.send(dbfarr);
 });
 
 app.listen(port);
@@ -57,21 +53,3 @@ app.listen(port);
   pool.end();
 });
 
-//connection.connect(function(err) {
-  //if (err) throw err;
-  //connection.query("select * from till_buttons;", function (err, result, fields) {
-    //if (err) throw err;
-    //tableinf = result;
-  //});
-//});
-
-//console.log(tableinf[0])
-
-//var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
-
-//app.use(express.static(__dirname + '/public')); //Serves the web pages
-//app.get("/buttons",function(req,res){ // handles the /buttons API
-  //res.send(buttons);
-//});
-
-//app.listen(port);
