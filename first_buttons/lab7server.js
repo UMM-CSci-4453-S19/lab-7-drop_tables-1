a65bd5c44a796f512c3b218d0e906be09574c99a
Ethan Uphoff <ethan.uphoff@gmail.com>
2019-03-06 08:09:10 -0600

    connect server to database

--- first_buttons/lab7server.js
+++ first_buttons/lab7server.js
@@ -1,6 +1,21 @@
 var express=require('express'),
 app = express(),
 port = process.env.PORT || 1337;
+var credentials = require('./credentials.json');
+
+var mysql=require("mysql");
+
+credentials.host="ids"
+credentials.database = "create_tables"
+var connection = mysql.createConnection(credentials);
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
 
 var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
 
