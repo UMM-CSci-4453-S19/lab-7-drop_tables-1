fbdea00fff55d86cdb1e32cbd5327d294f5e474d
EthanUphoff <ethan.uphoff@gmail.com>
2019-03-06 11:03:29 -0600

    Properly get data and format from sql table

--- first_buttons/lab7server.js
+++ first_buttons/lab7server.js
@@ -31,8 +31,21 @@ var query = function(command) {
 
 var result = query("select * from till_buttons;");
 result.then(function(dbfs,err){
+  var dbfarr = new Array(dbfs.length);
   console.log(dbfs[0].label)
+  dbfs.forEach(function (item, index) {
+  	console.log(item.leftPosition)
+	dbfarr[index] = {"buttonID":item.button_id,
+		"left":item.leftPosition,
+		"top":item.topPosition,
+		"width":item.wide,
+		"label":item.label,
+		"invID":item.invID};
+	console.log(dbfarr[index])
+  })
   var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
+
+console.log(buttons[0])
 
 app.use(express.static(__dirname + '/public')); //Serves the web pages
 app.get("/buttons",function(req,res){ // handles the /buttons API
