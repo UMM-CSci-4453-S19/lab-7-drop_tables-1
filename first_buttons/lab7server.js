a9d1d43abce539c52a65069bc8442240b6740331
EthanUphoff <ethan.uphoff@gmail.com>
2019-03-06 10:40:57 -0600

    app properly running in pool

--- first_buttons/lab7server.js
+++ first_buttons/lab7server.js
@@ -31,22 +31,8 @@ var query = function(command) {
 
 var result = query("select * from till_buttons;");
 result.then(function(dbfs,err){
-  console.log(dbfs.length)
-}).then(function(){
-  pool.end();
-});
-
-connection.connect(function(err) {
-  if (err) throw err;
-  connection.query("select * from till_buttons;", function (err, result, fields) {
-    if (err) throw err;
-    tableinf = result;
-  });
-});
-
-console.log(tableinf[0])
-
-var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
+  console.log(dbfs[0].label)
+  var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
 
 app.use(express.static(__dirname + '/public')); //Serves the web pages
 app.get("/buttons",function(req,res){ // handles the /buttons API
@@ -54,3 +40,25 @@ app.get("/buttons",function(req,res){ // handles the /buttons API
 });
 
 app.listen(port);
+}).then(function(){
+  pool.end();
+});
+
+//connection.connect(function(err) {
+  //if (err) throw err;
+  //connection.query("select * from till_buttons;", function (err, result, fields) {
+    //if (err) throw err;
+    //tableinf = result;
+  //});
+//});
+
+//console.log(tableinf[0])
+
+//var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
+
+//app.use(express.static(__dirname + '/public')); //Serves the web pages
+//app.get("/buttons",function(req,res){ // handles the /buttons API
+  //res.send(buttons);
+//});
+
+//app.listen(port);
